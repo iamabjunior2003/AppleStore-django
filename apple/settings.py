89f100eb24d421956ecd76a91a3620d336d0b175
AB <75667240+iamabjunior2003@users.noreply.github.com>
2026-02-08 11:08:14 +0530

    setting update

--- apple/settings.py
+++ apple/settings.py
@@ -74,16 +74,24 @@ WSGI_APPLICATION = 'apple.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'apple_products',
-        'USER': 'root',
-        'PASSWORD': 'Aditya@0229',
-        'HOST': 'localhost',
-        'PORT': '3306',
+if 'GITHUB_ACTIONS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'your_db',
+            'USER': 'your_user',
+            'PASSWORD': 'your_password',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
 
 
 # Password validation
