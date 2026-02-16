310d1b2339b1240f315fd67c56c43d345d15bfc7
AB <75667240+iamabjunior2003@users.noreply.github.com>
2026-02-16 14:47:32 +0530

    Update template directory path syntax in settings.py

--- apple/settings.py
+++ apple/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'apple.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,6 +79,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 
 WSGI_APPLICATION = 'apple.wsgi.application'
