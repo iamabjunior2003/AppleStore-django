5895d798a84d205ea6cd52c2dd842f22e2f81b6e
AB <75667240+iamabjunior2003@users.noreply.github.com>
2026-02-16 14:39:31 +0530

    Change DEBUG setting to True
    Set DEBUG to True for development environment.

--- apple/settings.py
+++ apple/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Render automatically sets this
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
